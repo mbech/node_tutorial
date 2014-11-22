0b05a4954e3b0b537b9e079908c869231e345128
Matt Bechtel <matt.b.bechtel@gmail.com>
2014-11-21 20:35:32 -0800

    Refactor to streamline retrieval of filepath arg

--- program.js
+++ program.js
@@ -1,9 +1,6 @@
 var fs = require('fs');
 
+var textBuffer = fs.readFileSync(process.argv[2]).toString();
 
-var filePath = process.argv.slice(2).shift();
-
-var textBuffer = fs.readFileSync(filePath);
-
-console.log(textBuffer.toString().split('\n').length - 1);
+console.log(textBuffer.split('\n').length - 1);
 
