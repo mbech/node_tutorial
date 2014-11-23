ee997baf546d1c99d7da64ef878258b411971d70
Matt Bechtel <matt.b.bechtel@gmail.com>
2014-11-22 19:48:35 -0800

    Solution to filtered ls passing

--- program.js
+++ program.js
@@ -1,8 +1,13 @@
 var fs = require('fs');
+var filepath = process.argv[2];
+var extension = process.argv[3];
 
-fs.readFile(process.argv[2], function (err, fileContents){
-  var textBuffer = fileContents.toString();
-  console.log(textBuffer.split('\n').length - 1);
+fs.readdir(filepath, function (err, fileNameArray){
+  var extRegex = new RegExp("^.+\." + extension + "$");
+  var filteredNameArray = fileNameArray.filter(function(element){
+    return extRegex.test(element);
+  });
+  console.log(filteredNameArray.join("\n"));
 });
 
 
