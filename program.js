60a87f6826c7e145357f0d26d08af3d9d7711159
Matt Bechtel <matt.b.bechtel@gmail.com>
2014-11-22 19:15:05 -0800

    Challenge passed

--- program.js
+++ program.js
@@ -1,6 +1,8 @@
 var fs = require('fs');
 
-var textBuffer = fs.readFileSync(process.argv[2]).toString();
+fs.readFile(process.argv[2], function (err, fileContents){
+  var textBuffer = fileContents.toString();
+  console.log(textBuffer.split('\n').length - 1);
+});
 
-console.log(textBuffer.split('\n').length - 1);
 
