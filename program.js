f33426704d8110dd9345522b9d07a7b9eec597ce
Matt Bechtel <matt.b.bechtel@gmail.com>
2014-11-23 19:26:25 -0800

    More aggressive simplification of solution

--- program.js
+++ program.js
@@ -1,17 +1,8 @@
 var http = require("http");
 
 http.get(process.argv[2], function(response){
-  var str = "";
-  response.on('data', function(responseChunk){
-    str += responseChunk + "\n";
-  });
-  response.on('end', function(){
-    console.log(str);
-  });
-  response.on('error', function(){
-    console.log("something went terribly wrong...");
-  });
-
+  response.setEncoding("utf8");
+  response.on('data', console.log);
 });
 
 
