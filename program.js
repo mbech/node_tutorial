7a9dc35612fc3ca2f93d3f4ca3dddd8e6c91c41a
Matt Bechtel <matt.b.bechtel@gmail.com>
2014-11-24 20:58:20 -0800

    Initial solution, tests passing

--- program.js
+++ program.js
@@ -1,8 +1,15 @@
 var http = require("http");
 
 http.get(process.argv[2], function(response){
+  var contents = "";
   response.setEncoding("utf8");
-  response.on('data', console.log);
+  response.on('data', function(chunk){
+    contents += chunk;
+  });
+  response.on('end', function(){
+    console.log(contents.length);
+    console.log(contents);
+  });
 });
 
 
