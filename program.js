999344fc7f52376d693dffd46f90a09bcd339217
Matt Bechtel <matt.b.bechtel@gmail.com>
2014-11-26 15:39:52 -0800

    Refactor to reduce variable clutter

--- program.js
+++ program.js
@@ -5,13 +5,11 @@ var server = net.createServer(function (socket) {
   var date = new Date;
 
   //Format should be: "YYYY-MM-DD hh:mm"
-  var dateStr = "";
-
-  dateStr += date.getFullYear() + "-";
-  dateStr += ("0" + (date.getMonth() + 1)).slice(-2) + "-";  // starts at 0
-  dateStr += ("0" + date.getDate()).slice(-2) + " ";  // starts at 0
-  dateStr += ("0" + date.getHours()).slice(-2) + ":";
-  dateStr += ("0" + date.getMinutes()).slice(-2);
+  var dateStr = date.getFullYear() + "-" +
+  ("0" + (date.getMonth() + 1)).slice(-2) + "-" +  // starts at 0
+  ("0" + date.getDate()).slice(-2) + " " + 
+  ("0" + date.getHours()).slice(-2) + ":" +
+  ("0" + date.getMinutes()).slice(-2);
 
   socket.end(dateStr);
 })
