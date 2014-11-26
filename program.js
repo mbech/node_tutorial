8a9dde6c9b6fa04b3c7d535d23bc310460814fb0
Matt Bechtel <matt.b.bechtel@gmail.com>
2014-11-25 19:22:19 -0800

    Aync solution to challenge

--- program.js
+++ program.js
@@ -1,11 +1,10 @@
 var http = require("http");
 var urls = [];
+var results = [];
+var count = 0;
 
-var httpGetQueue = function(url){
-  if(!url){
-    return 0
-  }
-
+var httpGetQueue = function(index){
+  var url = urls[index];
   http.get(url, function(response){
     var contents = "";
     response.setEncoding("utf8");
@@ -13,12 +12,19 @@ var httpGetQueue = function(url){
       contents += chunk;
     });
     response.on('end', function(){
-      console.log(contents);
-      httpGetQueue(urls.shift());
+      results[index] = contents;
+      count += 1;
+      if(count === urls.length ){
+        console.log(results.join("\n"));
+      }
     });
   });
 };
 
 urls = process.argv.slice(2);
+var len = urls.length;
 
-httpGetQueue(urls.shift());
+for(var i = 0; i < len; i++){
+  httpGetQueue(i);
+}
+
