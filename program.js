7c9469472a7ae7839d9d29f40d3c8d657712e4ee
Matt Bechtel <matt.b.bechtel@gmail.com>
2014-11-30 14:19:46 -0800

    Initial solution incomplete, tests freezing

--- program.js
+++ program.js
@@ -1,11 +1,23 @@
 var portNum = process.argv[2];
 var http = require('http');
-var map = require('through2-map');
+var url = require('url');
 
 var server = http.createServer(function (req, res){
-  req.pipe(map(function(chunk){
-    return chunk.toString().toUpperCase();
-  })).pipe(res);
+  var pathName = url.parse(req.url).pathname;
+  var query = url.parse(req.url, true).query;
+  var queryDate = new Date(query.iso);
+  var responseObj = {};
+  console.log("here");
+
+  if (pathName === '/api/parsetime'){
+    responseObj.hour = queryDate.getHours();
+    responseObj.minute = queryDate.getMinutes(); 
+    responseObj.second = queryDate.getSeconds(); 
+  } else if (pathName === '/api/unixtime'){
+    responseObj.unixtime = queryDate.getTime();
+  } 
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.write(JSON.stringify(responseObj));
 });
 
 server.listen(portNum);
